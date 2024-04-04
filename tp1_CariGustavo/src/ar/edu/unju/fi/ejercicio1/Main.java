91cae6313d5dd57ac63ec9877f82e74e02db6ac4
lara <lara@DESKTOP-EV4SE09>
2024-04-04 16:28:20 -0300

    inicio del proyecto tp1

---
+++ tp1_CariGustavo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float primerNumero = 4;
+		float segundoNumero = 6;
+		float tercerNumero = 7;
+		float cuartoNumero = 30;
+		float quintoNumero = 7;
+		float promedio = (primerNumero + segundoNumero + tercerNumero + cuartoNumero + quintoNumero)/ 5;
+		System.out.println("El promedio es: "+ promedio );
+	}
+
+}
